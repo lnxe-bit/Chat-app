a293ef7cd6899c3895e20bd5ddc92e741dc87851
lnxe-bit <lance.yzn@gmail.com>
2023-04-18 04:54:37 +0800

    final

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Chat-app
 Internship Assessment
+
+TO RUN THE PROGRAM:
+
+- Clone the code using "git clone" 
+
+- Install The Dependencies check it out in package.json file.
+
+- For the backend just run "npm start" on terminal. same as for the frontend.
---
+++ frontend/src/App.js
@@ -0,0 +1,44 @@
+import "./App.css";
+import io from "socket.io-client";
+import { useState } from "react";
+import Chat from "./Chat";
+
+const socket = io("http://localhost:3001");
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+      setShowChat(true);
+    }
+  };
+
+  return (
+    <div className="App">
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <h3>Chat.io</h3>
+          <input
+            type="text"
+            placeholder="Username"
+            onChange={(event) => setUsername(event.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Chat ID"
+            onChange={(event) => setRoom(event.target.value)}
+          />
+          <button onClick={joinRoom}>Join</button>
+        </div>
+      ) : (
+        <Chat socket={socket} username={username} room={room} />
+      )}
+    </div>
+  );
+}
+
+export default App;
